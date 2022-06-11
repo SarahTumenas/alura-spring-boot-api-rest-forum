e969dc921673e74ff002c0985bba663319a2ec01
Sarah Tumenas <sarah.marques@spread.com.br>
2022-06-11 10:07:15 -0300

    Trabalhando com Post

--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -19,16 +19,22 @@ public class Topico {
 	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
 
 	@ManyToOne
-
 	private Usuario autor;
 	@ManyToOne
-
 	private Curso curso;
 
 	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
 
 
+	public Topico() {
+	}
+
+	public Topico(String titulo, String mensagem, Curso curso) {
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
 
 	@Override
 	public int hashCode() {
