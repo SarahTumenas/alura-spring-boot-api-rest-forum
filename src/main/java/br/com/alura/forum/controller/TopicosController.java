9fe9633f6ca3e142d9ba78e5b1d92842d63e7a2a
Sarah Tumenas <sarah.marques@spread.com.br>
2022-06-11 11:48:39 -0300

    Método Delete

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -45,6 +45,7 @@ public class TopicosController {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity <TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 
         Topico topico = form.converter(cursoRepository);
@@ -72,6 +73,17 @@ public class TopicosController {
         Topico topico = form.atualizar(id, topicoRepository);
 
         return ResponseEntity.ok(new TopicoDto(topico));
+    }
+
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id) {
+
+            topicoRepository.deleteById(id);
+
+            return ResponseEntity.ok().build();
+
     }
 
 }
