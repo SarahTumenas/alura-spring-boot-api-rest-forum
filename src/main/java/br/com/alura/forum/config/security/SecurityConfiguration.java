dd29468ef33086a232c2d7daec88d9a0e2de86cf
Sarah Tumenas <sarah.marques@spread.com.br>
2022-06-15 11:44:09 -0300

    Restringindo acesso a endpoints privados

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -30,7 +30,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                .antMatchers(HttpMethod.POST, "/topicos/*").permitAll();
+                .antMatchers(HttpMethod.POST, "/topicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
 
     }
 }
