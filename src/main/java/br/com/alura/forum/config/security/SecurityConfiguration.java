673e0e2d3faa668af2df006a5e79856dee236593
Sarah Tumenas <sarah.marques@spread.com.br>
2022-06-15 12:06:36 -0300

    Autenticação do usuário

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,22 +8,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
     //configuração de autenticação
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-    }
-
-    //configuração de recursos estáticos(CSS, JS, imagens)
-    @Override
-    public void configure(WebSecurity web) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 
     }
+
 
 
     //configuração de autorização
@@ -35,5 +37,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and().formLogin();
 
     }
+
+    //configuração de recursos estáticos(CSS, JS, imagens)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println(new BCryptPasswordEncoder().encode("123456"));
+//    }
 }
 
