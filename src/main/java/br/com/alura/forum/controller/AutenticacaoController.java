5b96135b042469712b4bfa73ac5a6cebdb3734b5
Sarah Tumenas <sarah.marques@spread.com.br>
2022-06-15 14:10:48 -0300

    Gerando Tokens com JWT

---
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -0,0 +1,45 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.config.security.TokenService;
+import br.com.alura.forum.controller.form.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity <?> autenticar(@RequestBody @Valid LoginForm form) {
+
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+
+        try {
+            Authentication authenticate = authenticationManager.authenticate(dadosLogin);
+
+            String token = tokenService.gerarToken(authenticate);
+            System.out.println(token);
+
+            return ResponseEntity.ok().build();
+
+        }catch (AuthenticationException e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+
+    }
+}
