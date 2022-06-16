8e7a6794d3bdd48c5ad7a576f8baa40aa48afcf0
Sarah Tumenas <sarah.marques@spread.com.br>
2022-06-16 13:23:41 -0300

    Testendo um Repository com outro banco de dados

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,20 +5,34 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)  //pra configurar banco, sem ser o de memória para testes
 public class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository cursoRepository;
 
+    @Autowired
+    private TestEntityManager entityManager;
+
     @Test
     public void deveCarregarUmCursoAoBuscarPeloNome() {
-         String nomeCurso = "Spring Boot";
+         String nomeCurso = "HTML 5";
+
+         Curso html5 = new Curso();
+            html5.setNome(nomeCurso);
+            html5.setCategoria("Programação");
+            entityManager.persist(html5);
+
         Curso curso = cursoRepository.findByNome(nomeCurso);
         Assert.assertNotNull(curso);
         Assert.assertEquals(nomeCurso, curso.getNome());
